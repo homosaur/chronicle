99beb13472ee0bc35aa10435dc3cb08f772b55df
Paul Graham <paul@bytefair.com>
2016-07-17 04:00:22 -0500

    set up testing framework

---
+++ test/article.js
@@ -0,0 +1,9 @@
+describe('Article', function () {
+  it('should be able to be created')
+  it('should be able to be saved')
+  it('should have a title')
+  it('should have a slug')
+  it('should be able to be versioned')
+  it('should publish latest unless specified')
+  it('should respect published version')
+})
---
+++ test/collection.js
@@ -0,0 +1,5 @@
+describe('Collection', function () {
+  it('should be able to be created')
+  it('should be able to be assigned as a child')
+  it('should return children')
+})
